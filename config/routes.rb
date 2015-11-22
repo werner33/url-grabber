c59fdf7ced55b63d62f6b8050c3efe86f9a3dff3
JORDAN M MANLEY <jordanmmanley@JORDANs-MacBook-Air.local>
2015-11-21 21:50:05 -0500

    home page and basic show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root "pages#home"
+  post "show" => "pages#show"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
